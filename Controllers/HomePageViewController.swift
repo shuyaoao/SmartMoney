0f11a8ee618880078e829e35672e00760703f586
shuyao <shuyao27@gmail.com>
2023-05-19 13:21:14 +0800

    Add Launch Screen and Main Storyboard

---
+++ Controllers/HomePageViewController.swift
@@ -0,0 +1,16 @@
+
+//  SmartMoney
+//
+//  Created by Shuyao Li on 19/5/23.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+}
--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(red: 0.96, green: 0.95, blue: 0.95, alpha: 1.00)
         // Do any additional setup after loading the view.
     }
 
