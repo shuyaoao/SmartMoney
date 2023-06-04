070c190c9e273129693ca3c60caf18f33a3cda1f
shuyao <shuyao27@gmail.com>
2023-06-04 18:33:38 +0800

    Update GroupCell View

---
+++ Models/Group.swift
@@ -0,0 +1,8 @@
+//
+//  Group.swift
+//  SmartMoney
+//
+//  Created by Shuyao Li on 4/6/23.
+//
+
+import Foundation
---
+++ Models/GroupTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupTableViewCell.swift
+//  SmartMoney
+//
+//  Created by Shuyao Li on 4/6/23.
+//
+
+import UIKit
+
+class GroupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
