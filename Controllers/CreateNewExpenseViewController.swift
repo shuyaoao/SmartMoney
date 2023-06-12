1d476cfc93327da6bf89ad0d8264d4558e1890c3
dylanlo8 <lojingjiedylan@gmail.com>
2023-06-12 21:02:38 +0800

    Minor CreateNewExpense edits

--- Controllers/CreateNewExpenseViewController.swift
+++ Controllers/CreateNewExpenseViewController.swift
@@ -16,14 +16,32 @@ class CreateNewExpenseViewController: UIViewController {
     }
     
     
+    /*
+     struct Transaction: Identifiable {
+         let id: Int
+         let name: String
+         let date : String
+         let category : String
+         let amount : Double
+         let isExpense : Bool // if false, then income
+     }
+
+     */
+    
     // MARK: Terminating Buttons
-    
-    
     @IBAction func cancelButtonPressed(_ sender: UIButton) {
         // Closes Popup
         dismiss(animated: true)
     }
     
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        
+        // Create Transaction Data
+        // let newTransaction = Transaction(name: <#T##String#>, date: <#T##String#>, category: <#T##String#>, amount: <#T##Double#>, isExpense: <#T##Bool#>)
+        
+        // Close Popup
+        dismiss(animated: true)
+    }
     
 
 }
