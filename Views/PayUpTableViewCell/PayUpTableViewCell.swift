090d02e39fe7955704d3fc2d87dd9aecda21a8d1
shuyao <shuyao27@gmail.com>
2023-06-12 18:28:23 +0800

    Update PayUpPage and BalancesPage

---
+++ Views/PayUpTableViewCell/PayUpTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PayUpTableViewCell.swift
+//  SmartMoney
+//
+//  Created by Shuyao Li on 12/6/23.
+//
+
+import UIKit
+
+class PayUpTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
