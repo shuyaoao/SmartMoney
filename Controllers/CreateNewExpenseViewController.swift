2ec33ce6ac71dc89c59527346595174bf505dbdb
dylanlo8 <lojingjiedylan@gmail.com>
2023-06-14 18:55:28 +0800

    Made some edits

--- Controllers/CreateNewExpenseViewController.swift
+++ Controllers/CreateNewExpenseViewController.swift
@@ -12,8 +12,6 @@ class CreateNewExpenseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     
