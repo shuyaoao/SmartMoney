f6f55c05da15f8805fba48f97cccbf1826cf7cf9
dylanlo8 <lojingjiedylan@gmail.com>
2023-06-18 21:03:53 +0800

    fixed Expense button and Incomebutton

--- Controllers/CreateNewExpenseViewController.swift
+++ Controllers/CreateNewExpenseViewController.swift
@@ -18,16 +18,18 @@ class CreateNewExpenseViewController: UIViewController {
     var categorySelectedLabelvar = ""
     
     // UI Buttons
-    @IBOutlet weak var incomeButton: UIButton!
-    @IBOutlet weak var expenseButton: UIButton!
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var transactionNameTextField: UITextField!
+    @IBOutlet weak var expenseButton: UIButton!
+    @IBOutlet weak var incomeButton: UIButton!
     
     var alertController: UIAlertController?
         
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        self.expenseButton.isHighlighted = false
+        self.incomeButton.isHighlighted = false
+        
         // Expense Preselected
         newTransaction.isExpense = true
         
@@ -53,13 +55,13 @@ class CreateNewExpenseViewController: UIViewController {
     }
     
     @IBAction func saveButtonPressed(_ sender: UIButton) {
-        
         // Category Selection
         for index in 0..<catDataSource.listofCategories.count {
             if catDataSource.listofCategories[index].selected == true {
                 newTransaction.category = catDataSource.listofCategories[index]
             }
             
+            // Reset all Category Buttons
             catDataSource.listofCategories[index] = catDataSource.listofCategories[index].buttonunSelected()
         }
         
@@ -69,8 +71,7 @@ class CreateNewExpenseViewController: UIViewController {
         // new Unique id of Transaction
         let id = transactionDataModel.transactionDataList.count
         
-        // Unwrapping of Optionals
-        // Checks if any of the Optionals are empty
+        // Checks if any of the datafields are empty
         // if Empty: Show a Warning Message
         if let name = newTransaction.name,
            let date = newTransaction.date,
@@ -95,12 +96,7 @@ class CreateNewExpenseViewController: UIViewController {
             // if any of the field has not been specified
             // Present the alert controller
             showAlert()
-            
-            
         }
-        
-
-        
     }
     @IBAction func transactNameChanged(_ sender: UITextField) {
         newTransaction.name = transactionNameTextField.text
@@ -111,17 +107,18 @@ class CreateNewExpenseViewController: UIViewController {
         newTransaction.name = transactionNameTextField.text
     }
 
+    //newTransaction.isExpense = true
     // Income and Expense Buttons
-    @IBAction func expenseButtonSelected(_ sender: UIButton) {
-        expenseButton.backgroundColor = .systemTeal
-        incomeButton.backgroundColor = .white
+    @IBAction func expenseButtonClicked(_ sender: UIButton) {
         newTransaction.isExpense = true
+        self.incomeButton.tintColor = .lightGray
+        self.expenseButton.tintColor = .systemTeal
     }
     
-    @IBAction func incomeButtonSelected(_ sender: UIButton) {
-        sender.backgroundColor = .systemTeal
-        expenseButton.backgroundColor = .white
+    @IBAction func incomeButtonClicked(_ sender: UIButton) {
         newTransaction.isExpense = false
+        self.incomeButton.tintColor = .systemTeal
+        self.expenseButton.tintColor = .lightGray
     }
     
     // Category Scroll View
@@ -153,6 +150,8 @@ class CreateNewExpenseViewController: UIViewController {
         alertController?.dismiss(animated: true, completion: nil)
         alertController = nil
     }
+    
+    
     
 }
 
